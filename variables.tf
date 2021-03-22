4b28a0123fb5e2bd9a0c4035a97d243ed230b809
martijnvdp <m.vanderploeg@nl.ccv.eu>
2021-03-22 14:07:52 +0100

    add resource limits

--- variables.tf
+++ variables.tf
@@ -1,23 +1,7 @@
-variable "agent_name" {
-  type        = string
-  description = "The TFC agent token description defined in TFC at app/<org>/settings/agents."
-}
-
-variable "agent_token" {
-  type        = string
-  description = "The TFC agent token generated when the agent was created."
-}
-
-variable "tfc_url" {
-  type        = string
-  default     = "https://app.terraform.io"
-  description = "The Terraform Cloud endpoint.  Must be changed if using Terraform Enterprise."
-}
-
-variable "agent_log_level" {
-  type        = string
-  default     = "error"
-  description = "Available log levels are info, error, warn, debug, and trace."
+variable "agent_disable_update" {
+  type        = bool
+  default     = true
+  description = "Agents will self-update if set to false."
 }
 
 variable "agent_image" {
@@ -26,16 +10,15 @@ variable "agent_image" {
   description = "Name of the Terraform Cloud Agent docker image."
 }
 
-variable "agent_version" {
+variable "agent_log_level" {
   type        = string
-  default     = "latest"
-  description = "Version of the Terraform Cloud Agent docker image."
+  default     = "error"
+  description = "Available log levels are info, error, warn, debug, and trace."
 }
 
-variable "agent_disable_update" {
-  type        = bool
-  default     = true
-  description = "Agents will self-update if set to false."
+variable "agent_name" {
+  type        = string
+  description = "The TFC agent token description defined in TFC at app/<org>/settings/agents."
 }
 
 variable "agent_replicacount" {
@@ -44,9 +27,15 @@ variable "agent_replicacount" {
   description = "Replicacount of the terraform cloud agent deployment."
 }
 
-variable "namespace" {
+variable "agent_token" {
   type        = string
-  description = "The namespace to deploy the agent into.  Unless create_namespace is true, the namespace must already exist."
+  description = "The TFC agent token generated when the agent was created."
+}
+
+variable "agent_version" {
+  type        = string
+  default     = "latest"
+  description = "Version of the Terraform Cloud Agent docker image."
 }
 
 variable "cluster_access" {
@@ -60,3 +49,39 @@ variable "create_namespace" {
   default     = false
   description = "When true, creates the namespace for the Terraform Cloud Agent."
 }
+
+variable "limits_cpu" {
+  type        = string
+  default     = "1000m"
+  description = "CPU hard limits."
+}
+
+variable "limits_memory" {
+  type        = string
+  default     = "1Gb"
+  description = "Memory hard limits."
+}
+
+variable "namespace" {
+  type        = string
+  description = "The namespace to deploy the agent into.  Unless create_namespace is true, the namespace must already exist."
+}
+
+variable "requests_cpu" {
+  type        = string
+  default     = "1"
+  description = "CPU requests."
+}
+
+variable "requests_memory" {
+  type        = string
+  default     = "1Gb"
+  description = "Memory requests."
+}
+
+variable "tfc_url" {
+  type        = string
+  default     = "https://app.terraform.io"
+  description = "The Terraform Cloud endpoint.  Must be changed if using Terraform Enterprise."
+}
+
