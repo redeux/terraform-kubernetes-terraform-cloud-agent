afd81a4eab563a65b7049e43c06a020af3e71c63
Adam Buxton <buxton.adam@gmail.com>
2022-02-04 16:30:09 +0000

    update to test

--- test/default/main.tf
+++ test/default/main.tf
@@ -1,10 +1,13 @@
 
 module "terraform-cloud-agent-kubernetes" {
-  source  = "redeux/terraform-cloud-agent/kubernetes"
-  version = "0.1.0"
-  namespace          = "terraform-cloud-agent"
-  create_namespace   = true
-  agent_name         = "example-agent"
-  agent_token        = var.agent_token
-  cluster_access     = true
+  #source           = "redeux/terraform-cloud-agent/kubernetes"
+  source           = "../../"
+  #version          = "0.1.0"
+  namespace        = "terraform-cloud-agent"
+  create_namespace = true
+  agent_name       = "example-agent"
+  agent_token      = var.agent_token
+  cluster_access   = true
+  agent_version    = var.agent_version
+
 }
--- test/default/variables.tf
+++ test/default/variables.tf
@@ -3,3 +3,8 @@ variable "agent_token" {
   description = "The TFC agent token generated when the agent was created."
   sensitive   = true
 }
+variable "agent_version" {
+  type        = string
+  default     = "latest"
+  description = "Version of the Terraform Cloud Agent docker image."
+}
