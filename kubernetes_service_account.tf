41bb15f0fd465475221de339ff233e23e11448a0
niroz89 <niro@sph.com.sg>
2022-08-17 02:19:43 +0800

    Introduce variable for service account name, follow variable attributes ordering similar as upstream

--- kubernetes_service_account.tf
+++ kubernetes_service_account.tf
@@ -3,12 +3,12 @@ resource "kubernetes_service_account" "tfc_agent_service_account" {
 
   metadata {
     labels = {
-      "app.kubernetes.io/name"           = "terraform-cloud-agent"
+      "app.kubernetes.io/name"           = var.service_account_name
       "app.kubernetes.io/module-version" = local.module-version
       "app.kubernetes.io/managed-by"     = "terraform"
     }
-    name        = "terraform-cloud-agent"
+    name        = var.service_account_name
     namespace   = var.create_namespace ? kubernetes_namespace.tfc_agent_namespace[0].metadata[0].name : var.namespace
-    annotations = var.service_account_annotations 
+    annotations = var.service_account_annotations
   }
 }
